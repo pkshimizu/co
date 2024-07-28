92330d553b12bfef071aee0b01dc0bea3687050e
shimizu.kenji <shimizu.kenji@noncore.net>
2024-07-28 20:28:53 +0900

    feat: load yaml file

--- cmd/ca/main.go
+++ cmd/ca/main.go
@@ -2,8 +2,46 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
 func main() {
 	fmt.Println("start ca")
+	loadCommands()
+}
+
+func loadCommands() Setting {
+	// 以下の順序で.ca.yamlファイルを読み込む
+	// 1. カレントディレクトリ
+	setting := loadYaml(".")
+	return setting
+	// 2. 上位ディレクトリ
+	// 3. CA_HOMEディレクトリ
+	// 4. ホームディレクトリ
+}
+
+type SettingCommand struct {
+	Exec        string `yaml:"exec"`
+	WorkingDir  string `yaml:"working_dir"`
+	Description string `yaml:"description"`
+}
+
+type Setting struct {
+	Commands map[string]SettingCommand `yaml:"commands"`
+}
+
+func loadYaml(dir string) Setting {
+	// yamlファイルを読み込む
+	setting := Setting{}
+	b, err := os.ReadFile(dir + "/.ca.yaml")
+	if err != nil {
+		return setting
+	}
+	err = yaml.Unmarshal(b, &setting)
+	if err != nil {
+		return setting
+	}
+	return setting
 }
