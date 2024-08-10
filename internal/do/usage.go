a4c954626c03daeea8316654d229cd3b74bd7624
shimizu.kenji <shimizu.kenji@noncore.net>
2024-08-10 20:33:07 +0900

    feat: display commands

--- internal/do/usage.go
+++ internal/do/usage.go
@@ -5,4 +5,20 @@ import "fmt"
 func Print(setting Setting) {
 	// ヘルプを表示する
 	fmt.Print("Usage: do <command> [<args>]\n\n")
+
+	fmt.Print("Commands\n")
+	ml := getMaxCommandNameLength(setting)
+	for _, cmd := range setting.Commands {
+		fmt.Printf("  %-*s  %s\n", ml, cmd.Name, cmd.Description)
+	}
+}
+
+func getMaxCommandNameLength(setting Setting) int {
+	l := 0
+	for _, cmd := range setting.Commands {
+		if len(cmd.Name) > l {
+			l = len(cmd.Name)
+		}
+	}
+	return l
 }
